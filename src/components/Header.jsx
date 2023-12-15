7bc4157f8754093549301a74ba038a9d277d3058
David12331 <dpantoja1@pride.hofstra.edu>
2023-12-15 00:15:09 -0500

    added nav

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import './Header.css';
+
+function Header() {
+
+  //an example of an inline style for the nav tag for an initial layout - later additions, or 
+
+
+
+  return (
+    <header>
+      <h1>My Portfolio Page</h1>
+      <nav className='navStyle'>
+        <Link to="/">
+          <div>HOME</div>
+        </Link>
+        <Link to="/about">
+          <div>ABOUT</div>
+        </Link>
+        <Link to="/projects">
+          <div>PROJECTS</div>
+        </Link>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
